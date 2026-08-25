1df6292d5315f9cb125b44c56feed5b073f8b27d
mspfloresclass-pf <mspfloresclass@gmail.com>
2026-08-25 20:58:22 +0000

    Lab 5

---
+++ lab5b.py
@@ -0,0 +1,17 @@
+#Patricia Flores
+#Create a list with 10 even numbers. Use different numbers than the sample output. 
+
+nums = [2,10,14,18,52,36,90,50,22,100]
+
+
+
+
+for i in nums:
+    print(i)
+
+
+
+
+
+
+
---
+++ lab5c.py
@@ -0,0 +1,17 @@
+#Patricia Flores
+#Create a list of five strings, each string must be a fruit or a vegetable. I chose a fruit 
+
+fruit = ["pear","plum","peach", "orange","strawberry"]
+
+
+
+
+for i in fruit:
+    print(i)
+
+
+
+
+
+
+
