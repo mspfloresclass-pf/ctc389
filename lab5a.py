afcaaca5e358f8fdec30afad66e761bbfb7fb48f
mspfloresclass-pf <mspfloresclass@gmail.com>
2026-08-25 20:31:09 +0000

    Lab 5 first part

---
+++ lab5a.py
@@ -0,0 +1,45 @@
+#Patricia Flores
+#Create a list with 10 even numbers. Use different numbers than the sample output. 
+
+nums = [2,10,14,18,52,36,90,50,22,100]
+
+print("first number")
+print(nums[0])
+
+print("second number")
+print(nums[1])
+
+print("third number")
+print(nums[2])
+
+print("fourth number")
+print(nums[3])
+
+
+print("fith number")
+print(nums[4])
+
+print("sixtht number")
+print(nums[5])
+
+print("seventh number")
+print(nums[6])
+
+
+print("eighth number")
+print(nums[7])
+
+
+print("ninth number")
+print(nums[8])
+
+
+print("tenth number")
+print(nums[9])
+
+
+
+
+
+
+
