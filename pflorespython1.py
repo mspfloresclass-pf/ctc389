188666a4f44625b7ca02cf64608936ee0d75747f
mspfloresclass-pf <mspfloresclass@gmail.com>
2026-09-10 00:42:28 +0000

    Python1

---
+++ pflorespython1.py
@@ -0,0 +1,12 @@
+#Patricia Flores
+
+
+
+user_name=input("What is your name?")
+user_age=int(input("Enter a number: "))
+add_number=int(input("How much would you like to add to the number?"))
+total=user_age + add_number
+print(user_name,"you entered",user_age,"and added",add_number,"to get a total of ",total)
+
+
+
